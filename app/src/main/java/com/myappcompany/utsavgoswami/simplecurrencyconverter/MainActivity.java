1ef02ebae0f34224baf209def55aa87bb35960ed
Utsav Goswami <utsavg19@googlemail.com>
2019-07-27 23:37:54 -0700

    Added List of Supported Currencies, User Input Tracking
    Also added two fields in MainActivity class to track user's selected currencies to convert to and from

--- app/src/main/java/com/myappcompany/utsavgoswami/simplecurrencyconverter/MainActivity.java
+++ app/src/main/java/com/myappcompany/utsavgoswami/simplecurrencyconverter/MainActivity.java
@@ -3,26 +3,65 @@ package com.myappcompany.utsavgoswami.simplecurrencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    public void convert(View view) {
-        EditText amount = (EditText) findViewById(R.id.userAmount);
-        double eurAmount = Double.parseDouble(amount.getText().toString());
-
-        // Hardcoded for now, will use API to reflect current FOREX values
-        double usdAmount = eurAmount * 1.3;
-
-        // Hardcoded in for now
-        Toast.makeText(this, "£" + eurAmount + " is $" + usdAmount, Toast.LENGTH_SHORT)
-                .show();
-    }
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    String selectedBaseCurrency;
+    String selectedConvertCurrency;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*Set Contents of Spinners*/
+        Spinner baseSpinner = (Spinner) findViewById(R.id.baseSpinner);
+        Spinner convertSpinner = (Spinner) findViewById(R.id.convertSpinner);
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        // createFromResource() method allows you to create an ArrayAdapter from the string array
+        // defined in currencies.xml
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.available_currencies, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        // The setAdapter() method applies the adapter to BaseSpinner
+        baseSpinner.setAdapter(adapter);
+        convertSpinner.setAdapter(adapter);
+
+        // Spinner click listeners
+        baseSpinner.setOnItemSelectedListener(this);
+        convertSpinner.setOnItemSelectedListener(this);
+
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+        String item = parent.getItemAtPosition(pos).toString();
+
+        // Reference: https://stackoverflow.com/questions/9262871/android-two-spinner-onitemselected
+        if (parent.getId() == R.id.baseSpinner) {
+            selectedBaseCurrency = item.substring(0, 3);
+            Toast.makeText(this, "User selected " +
+                    selectedBaseCurrency + " as base currency",
+                    Toast.LENGTH_SHORT).show();
+        } else { // Has to be convertedSpinner since there are only two spinners
+            selectedConvertCurrency = item.substring(0, 3);
+            Toast.makeText(this, "User selected " + selectedConvertCurrency
+            + " as converted currency", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
     }
 }
